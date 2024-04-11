6033a6084f92fe0f6170deb75a69a70f30dc7507
Golikov-Andrey <newsc2@yandex.ru>
2024-04-11 21:55:38 +0300

    Solution from seminar 1 Maven Gradle

---
+++ Seminar01/example3_sem1/test_app/src/main/java/ru/gb/Main.java
@@ -0,0 +1,13 @@
+package ru.gb;
+
+import com.google.common.base.Joiner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        String[] words = {"Hello", "World"};
+        String message = Joiner.on(", ").join(words);
+        System.out.println(message);
+
+    }
+}
